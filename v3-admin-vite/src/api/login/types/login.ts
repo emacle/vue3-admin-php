b1e11d16fa4a4b3ffea86fac94e4a5a5dcc80675
emacle <qkbeyond@gmail.com>
2024-04-15 23:24:53 +0800

    配合userStore后端获取信息, 添加ts变量类型

--- v3-admin-vite/src/api/login/types/login.ts
+++ v3-admin-vite/src/api/login/types/login.ts
@@ -11,4 +11,35 @@ export type LoginCodeResponseData = ApiResponseData<string>
 
 export type LoginResponseData = ApiResponseData<{ token: string }>
 
-export type UserInfoResponseData = ApiResponseData<{ username: string; roles: string[] }>
+// export type UserInfoResponseData = ApiResponseData<{ username: string; roles: string[] }>
+
+export interface CtrlPermData {
+  // 假设每个控制权限对象有以下属性
+  path: string
+}
+export interface metaData {
+  icon: string
+  title: string
+}
+export interface asyncRouterMapData {
+  // 假设每个控制权限对象有以下属性
+  component: string
+  hidden: number
+  id: number
+  listorder: number
+  name: string
+  path: string
+  pid: number
+  redirect: string
+  type: number
+  status: number
+  meta: metaData
+  children: asyncRouterMapData[]
+}
+
+export type UserInfoResponseData = ApiResponseData<{
+  username: string
+  roles: string[]
+  ctrlperm: CtrlPermData[]
+  asyncRouterMap: asyncRouterMapData[]
+}>
