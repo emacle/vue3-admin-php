5f1989cf333a078892648cd01ed44e7411ffc86e
emacle <qkbeyond@gmail.com>
2024-05-19 20:10:08 +0800

    添加 GithubCorner 组件

--- v3-admin-vite/src/views/dashboard/components/Admin.vue
+++ v3-admin-vite/src/views/dashboard/components/Admin.vue
@@ -1,6 +1,12 @@
+<script lang="ts" setup>
+// 如果有需要在 setup 中添加逻辑，可以在这里编写
+import GithubCorner from "@/components/GIthubCorner/index.vue"
+</script>
+
 <template>
   <div class="app-container center">
     <el-empty description="欢迎来到 admin 角色专属首页" />
+    <GithubCorner />
   </div>
 </template>
 
@@ -10,5 +16,19 @@
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.app-container {
+  position: relative;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.github-corner {
+  position: absolute;
+  top: 0;
+  right: 0;
 }
 </style>
