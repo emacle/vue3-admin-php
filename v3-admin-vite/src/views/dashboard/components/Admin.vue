e2c26e0c6a208362ae01b32a3377fd8700d6621f
emacle <qkbeyond@gmail.com>
2024-05-20 00:41:21 +0800

    首页添加 PanelGroup、BoxCard组件

--- v3-admin-vite/src/views/dashboard/components/Admin.vue
+++ v3-admin-vite/src/views/dashboard/components/Admin.vue
@@ -1,34 +1,122 @@
 <script lang="ts" setup>
 // 如果有需要在 setup 中添加逻辑，可以在这里编写
+import { ref } from "vue"
 import GithubCorner from "@/components/GIthubCorner/index.vue"
+import PanelGroup from "./PanelGroup.vue"
+import BoxCard from "./BoxCard.vue"
+
+const lineChartData = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+}
+
+const lineChartDataRef = ref(lineChartData.newVisitis)
+
+function handleSetLineChartData(type: string) {
+  // console.log("handleSetLineChartData type", type, lineChartData[type])
+  lineChartDataRef.value = lineChartData[type]
+}
 </script>
 
 <template>
-  <div class="app-container center">
-    <el-empty description="欢迎来到 admin 角色专属首页" />
+  <div class="dashboard-editor-container">
     <GithubCorner />
+
+    <PanelGroup @handleSetLineChartData="handleSetLineChartData" />
+
+    <!-- <el-row style="background: #fff; padding: 16px 16px 0; margin-bottom: 32px">
+      <line-chart :chart-data="lineChartData" />
+      line
+    </el-row>
+
+    <el-row :gutter="32">
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper"><raddar-chart /></div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper"><pie-chart /></div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper"><bar-chart /></div>
+      </el-col>
+    </el-row> -->
+
+    <el-row :gutter="8">
+      <el-col
+        :xs="{ span: 24 }"
+        :sm="{ span: 24 }"
+        :md="{ span: 24 }"
+        :lg="{ span: 12 }"
+        :xl="{ span: 12 }"
+        style="padding-right: 8px; margin-bottom: 30px"
+      >
+        <!-- <transaction-table /> -->
+        <box-card />
+      </el-col>
+      <el-col
+        :xs="{ span: 24 }"
+        :sm="{ span: 12 }"
+        :md="{ span: 12 }"
+        :lg="{ span: 6 }"
+        :xl="{ span: 6 }"
+        style="margin-bottom: 30px"
+      >
+        <!-- <todo-list /> -->
+        <box-card />
+      </el-col>
+      <el-col
+        :xs="{ span: 24 }"
+        :sm="{ span: 12 }"
+        :md="{ span: 12 }"
+        :lg="{ span: 6 }"
+        :xl="{ span: 6 }"
+        style="margin-bottom: 30px"
+      >
+        <box-card />
+      </el-col>
+    </el-row>
+    <el-empty description="欢迎来到 admin 角色专属首页" />
+
   </div>
 </template>
 
 <style lang="scss" scoped>
-.center {
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.app-container {
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
   position: relative;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+
+  .github-corner {
+    position: absolute;
+    top: 0px;
+    border: 0;
+    right: 0;
+  }
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
 }
 
-.github-corner {
-  position: absolute;
-  top: 0;
-  right: 0;
+@media (max-width: 1024px) {
+  .chart-wrapper {
+    padding: 8px;
+  }
 }
 </style>
